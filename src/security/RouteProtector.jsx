8d89045ba98d50d87a51ed1494c0abd30e04c843
Foster Amponsah <foster.asante@gwosevo.com>
2025-09-09 11:34:52 +0000

    Update

--- src/security/RouteProtector.jsx
+++ src/security/RouteProtector.jsx
@@ -1,11 +1,18 @@
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ isAllowed, redirectPath = "/login", children }) => {
-    if (!isAllowed) {
+const ProtectedRoute = ({ children, redirectPath = '/login' }) => {
+    //! Obtain login status from redux persist
+    const isAllowed = true; //! Get login status from redux persist
+    let user_role = 'staff'; //! Get the user role from redux persist
+    const roles = ['admin', 'staff', 'parent'];
+    let user_role_lower = user_role.toLocaleLowerCase();
+    // let redirectPath = "/login";
+
+    if (isAllowed && roles.includes(user_role_lower)) {
+        return children;
+    } else {
         return <Navigate to={redirectPath} replace />;
     }
-
-    return children;
 };
 
 export default ProtectedRoute;
