b45f62ec376236b38e5124884fe74c1dd6f8c21a
Foster Amponsah <foster.asante@gwosevo.com>
2025-09-18 10:38:09 +0000

    Update

--- src/screens/layouts/AdminLayout copy.jsx
+++ src/screens/layouts/AdminLayout copy.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import '../../styles/layouts/AdminLayout.css'
+import '../../styles/AdminLayout.css'
 import profile_image from '../../assets/images/bg.jpg';
 import { FaBullhorn, FaCreditCard, FaLock, FaSchool, FaUser } from 'react-icons/fa';
 import { FaGauge, FaListCheck, FaShieldHalved } from 'react-icons/fa6';
